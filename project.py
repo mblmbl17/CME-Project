044bf821de5a024de09c4c7b9ac1d6397d30e3b6
Griffin Stofko <gpstofko@cps.edu>
2024-01-25 11:18:36 -0600

    Linear Regression Working

--- project.py
+++ project.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-data = pd.read_csv("US Corn Futures Historical Data (1).csv")
+data = pd.read_csv("FINAL DATA.csv")
 y = data["Price"].values
-x = data["Change %"].values
+x = data["Date"].values
 
 x = x.reshape(-1,1)
 
@@ -17,13 +17,9 @@ coef = round(float(model.coef_), 2)
 intercept = round(float(model.intercept_), 2)
 r_squared = model.score(x, y)
 
-x_predict = 43
-
 # Print out the linear equation and r squared value
-prediction = model.predict([[x_predict]])
 print(f"Model's Linear Equation: y = {coef}x + {intercept}")
 print(f"R Squared value: {r_squared}")
-print(f"Prediction when x is {x_predict}: {prediction}")
 
 # Predict the the blood pressure of someone who is 43 years old.
 # Print out the prediction
@@ -31,7 +27,6 @@ print(f"Prediction when x is {x_predict}: {prediction}")
 plt.figure(figsize=(6,4))
 
 plt.scatter(x, y, c="purple")
-plt.scatter(x_predict, prediction, c="blue")
 plt.xlabel("Date")
 plt.ylabel("Price")
 plt.title("Price by Date")

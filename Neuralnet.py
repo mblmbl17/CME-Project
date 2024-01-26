eb9df61b11588daa71c9b181b109348f1aef5d75
Cristian <cnrosario@cps.edu>
2024-01-26 11:33:12 -0600

    Updated  the neural net.py hopefully the regression was implemented

--- Neuralnet.py
+++ Neuralnet.py
@@ -1,6 +1,32 @@
 import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 
+# Load data
+data = pd.read_csv("FINAL DATA.csv")
+y_regression = data["Price"].values
+x_regression = data["Date"].values.reshape(-1, 1)
+
+# Train regression model
+regression_model = LinearRegression().fit(x_regression, y_regression)
+
+# Extract coefficients
+coef_regression = regression_model.coef_[0]
+intercept_regression = regression_model.intercept_
+
+# Function to predict price using regression model
+def predict_price(date):
+    return coef_regression * date + intercept_regression
+
+# Prepare data for neural network
+X_nn = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y_nn = np.array([[0], [1], [1], [0]])
+
+# Add regression predictions as an additional feature to X_nn
+date_features = np.array([predict_price(date) for date in X_nn[:, 0]]).reshape(-1, 1)
+X_nn_with_regression = np.concatenate((X_nn, date_features), axis=1)
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
@@ -24,8 +50,8 @@ biases_hidden = np.random.uniform(size=(1, hidden_size))
 bias_output = np.random.uniform(size=(1, output_size))
 
 
-learning_rate = 0.12
-epochs = 5000
+learning_rate = 0.5
+epochs = 90000
 
 # Training the neural network
 for epoch in range(epochs):

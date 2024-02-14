6636cfd86eef739092bd33d0235f9e969ddcf8dc
Griffin Stofko <gpstofko@cps.edu>
2024-02-14 11:45:34 -0600

    changes

--- Basic Models/Fertilizer_V_Price.py
+++ Basic Models/Fertilizer_V_Price.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
+fig, ax1 = plt.subplots()
+
 data = pd.read_csv("Basic Models/FINAL DATA.csv")
 
 data2 = pd.read_csv("Basic Models/ModifiedImportData.csv")
@@ -11,11 +13,12 @@ x = data2["Year"].values
 y1 = data2["Dollars"].values
 y2 = data["Price"].values
 
+ax1.plot(x, y1)
+ax2 = ax1.twinx()
+
 print(x)
 print(y1)
 print(y2)
 
-plt.scatter(x, y1)
-plt.xlabel("Year")
-plt.ylabel("Dollars")
 plt.show()
+

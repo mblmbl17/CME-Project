bbeb5a7bc8b3027cbf147407c1a5f3450a3ea417
Griffin Stofko <gpstofko@cps.edu>
2024-02-15 11:29:13 -0600

    finalized fertilizer v price plotting

--- Basic Models/Fertilizer_V_Price.py
+++ Basic Models/Fertilizer_V_Price.py
@@ -9,16 +9,31 @@ fig, ax1 = plt.subplots()
 data = pd.read_csv("Basic Models/FINAL DATA.csv")
 
 data2 = pd.read_csv("Basic Models/ModifiedImportData.csv")
-x = data2["Year"].values
-y1 = data2["Dollars"].values
-y2 = data["Price"].values
 
-ax1.plot(x, y1)
+#only take data from 2004 to 2012
+x_rows = [0,1,2,3,4,5,6,7,8]
+y2_rows = [11,12,13,14,15,16,17,18,19]
+updatedData = data[data.index.isin(y2_rows)]
+updatedData2 = data2[data2.index.isin(x_rows)]
+x = updatedData2["Year"].values
+y1 = updatedData2["Dollars"].values
+y2 = updatedData["Price2"].values
+
+ax1.plot(x, y1, color = "blue")
 ax2 = ax1.twinx()
+ax2.plot(x, y2, color = "red")
 
-print(x)
-print(y1)
-print(y2)
+ax1.spines['right'].set_position(('axes',1.15))
+
+ax1.set_xlabel("Year")
+ax1.set_ylabel("Dollars", color = "blue")
+ax2.set_ylabel("Fertilizer Price", color = "red")
+
+ax1.tick_params(axis='y', colors = "blue")
+ax2.tick_params(axis='y', colors = "red")
+
+ax2.spines['left'].set_color("blue")
+ax2.spines['right'].set_color("red")
 
 plt.show()
 

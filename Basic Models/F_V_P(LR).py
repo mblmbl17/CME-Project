df982b20b0b3c9fe8bbdae3043876ca00b585f85
Griffin Stofko <gpstofko@cps.edu>
2024-02-16 11:20:50 -0600

    Linear Regression Works, but Line of Best Fit bad

--- Basic Models/F_V_P(LR).py
+++ Basic Models/F_V_P(LR).py
@@ -27,18 +27,18 @@ print(ytrain)
 print(ytest)
 xtrain = xtrain.reshape(-1, 1)
 print(xtrain)
-# model = LinearRegression().fit(xtrain, ytrain)
+model = LinearRegression().fit(xtrain, ytrain)
 
-# coef = round(float(model.coef_), 2)
-# intercept = round(float(model.intercept_), 2)
-# r_squared = model.score(xtrain, ytrain)
+coef = round(float(model.coef_), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(xtrain, ytrain)
 
-# print(f"Model's Linear Equation: y = {coef}x + {intercept}")
-# print(f"R Squared value: {r_squared}")
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
 
-# xtest = xtest.reshape(-1, 1)
-# predict = model.predict(xtest)
-# predict = np.around(predict, 2)
+xtest = xtest.reshape(-1, 1)
+predict = model.predict(xtest)
+predict = np.around(predict, 2)
 
 # print("\nTesting Linear Model with Testing Data:")
 # for index in range(len(xtest)):
@@ -47,15 +47,15 @@ print(xtrain)
 #    x_coord = xtest[index]
 #    print("x value:", float(x_coord), "Predicted y value:", predicted_y, "Actual y value:", actual)
 
-# plt.figure(figsize=(6,4))
+plt.figure(figsize=(6,4))
 
-# plt.scatter(xtrain, ytrain, c="purple", label="Training Data")
-# plt.scatter(xtest, ytest, c="blue", label="Testing Data")
-# plt.xlabel("Dollars")
-# plt.ylabel("Fertilizer Price")
-# plt.title("Dollars v Fertilizer Price")
+plt.scatter(xtrain, ytrain, c="purple", label="Training Data")
+plt.scatter(xtest, ytest, c="blue", label="Testing Data")
+plt.xlabel("Dollars")
+plt.ylabel("Fertilizer Price")
+plt.title("Dollars v Fertilizer Price")
 
-# plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
 
-# plt.legend()
-# plt.show()
+plt.legend()
+plt.show()
